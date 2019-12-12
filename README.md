5de0e1cf65359e9b58f0c719fefb93eb214c77a1
hejianxian <531601727@qq.com>
2019-12-12 16:33:33 +0800

    chore: open desktop application

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# open-desktop-application
- 通过 URL协议 打开桌面应用
+# @w6s/open-desktop-application
+
+> 通过 URL协议 打开桌面应用
+
+## 安装
+
+```bash
+yarn add @w6s/open-desktop-application
+// or
+npm install @w6s/open-desktop-application -S
+```
+
+可以直接通过`script`标签引入，全局暴露`openDesktopApplication`方法，使用方法和参数跟下面的一致。
+
+## 使用
+
+```js
+import openDesktopApplication from '@w6s/open-desktop-application';
+
+const params = {
+    protocol: 'workplus',
+    action: 'joinchat',
+    query: {
+        id: 1,
+        name: 'test',
+    },
+    fail: function() {},
+    success: function() {},
+};
+openDesktopApplication(params);
+```
+
+## 参数说明
+
+* protocol [必须]应用协议名，一般跟注册表有关系
+* action [必须]调用的方法名，需要和应用开发者进行约定
+* query 传入的参数，需要和应用开发者约定
+* fail 错误回调方法；不支持时，会返回`{ supported: false }`
+* success 成功回调方法
+
---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+import protocolDetection from 'custom-protocol-detection';
+import { stringify } from '@w6s/query-string';
+import isString from 'lodash.isstring';
+import isFunction from 'lodash.isstring';
+import isObject from 'lodash.isobject';
+
+/**
+ * open-desktop-application
+ *
+ * @param object { protocol, action, query, fail, success }
+ * 
+ * protocol: string (eg, workplus)
+ * action: string (eg, joinchat)
+ * query: object (eg, { id: 1, name: 'test' })
+ * fail: function
+ * success: function
+ *  
+ * return => workplus://joinchat/?id=1&name=test
+ */
+function openDesktopApplication(params) {
+    try {
+        const { protocol, action, query, fail, success } = params;
+        invariant(isString(protocol), '[protocol] Must be a non-empty string');
+        invariant(isString(action), '[action] Must be a non-empty string');
+        
+        let openUri = `${protocol}://${action}`;
+        if (isObject(query)) {
+            openUri += `?${stringify(query)}`;
+        }
+        protocolDetection(
+            openUri, 
+            function failCb(e) {
+                isFunction(fail) && fail(e);
+            },
+            function successCb() {
+                isFunction(success) && success();
+            },
+            function unsupportedCb() {
+                isFunction(fail) && fail({ supported: false });
+            }
+        );
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default openDesktopApplication;
